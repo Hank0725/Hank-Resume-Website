f8b719f6e8ed5a5060018b92107e6e9e2767024c
Hank <hank2929@hotmail.com>
2023-03-30 19:00:25 +0800

    add HomePage and Header、Footer components

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <h1>Hank Resume Website</h1>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Homepage />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Homepage from "./pages/Homepage.vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  pages: {
+    Homepage,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+body {
+  width: 100%;
 }
 </style>
---
+++ src/pages/Homepage.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus alias
+    ipsa eius incidunt modi voluptates quis earum adipisci ullam. Cumque, error
+    quidem. Unde consequuntur a magni voluptas nam, sint mollitia?
+  </p>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 10 rem;
+  color: blue;
+}
+</style>
